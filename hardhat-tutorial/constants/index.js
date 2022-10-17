fe9b83565bcce10f230317f79bdc488afc9b7eec
Astarota <toktamys.dosan@gmail.com>
2022-10-17 15:01:19 +0600

    first-init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0f9c2EAe38C89755D4E1f41412c31b440c9D5399";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
